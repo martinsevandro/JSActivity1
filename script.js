879a793a0ed0bc8cf20a66875034f3f06c3ada90
Evandro Filho Martins <71902902+martinsevandro@users.noreply.github.com>
2022-10-27 11:32:46 -0300

    Arquivos iniciados no Lab

---
+++ script.js
@@ -0,0 +1,36 @@
+//Array.isArray verifica se a entrada é array ou nao, true ou false, respectivamente
+const questao1 = ['item1', 'item2', 'item3', 'item4', 'item5'];
+console.log("Questão1: \nEntrada:", questao1," é Array?", Array.isArray(questao1));
+
+//slice é quem copia um array
+var clone = questao1.slice();
+console.log("\nQuestão2: \nArray da questão1: ", questao1, "\nSeu clone: ", clone);
+
+
+//Primeiro Elemento e Ns primeiros elementos
+console.log("\nQuestão3: \nArray da questão1: ", questao1, "\nExibir Elementos: ");
+PrimeiroElemento(questao1, 3);
+function PrimeiroElemento(array, n){
+    console.log("Primeiro Elemento: ", array[0]); //Primeiro Elemento
+    console.log("Exibir qtd de elementos com n = ", n);
+    for(let i=0; i<n; i++){
+        console.log((i+1),"º elemento: ", array[i]);
+    }
+}
+
+//Último Elemento e Ns últimos elementos
+console.log("\nQuestão4: \nArray da questão1: ", questao1, "\nExibir Elementos: ");
+UltimoElemento(questao1, 3);
+function UltimoElemento(array, n){
+    let ultimo = array[array.length - 1];
+    console.log("Ultimo Elemento: ", ultimo); //Primeiro Elemento
+    console.log("Exibir qtd de elementos com n = ", n);
+    for(let i=0; i<n; i++){        
+        console.log((array.length-(i)),"º elemento: ", array[array.length-(i+1)]);
+    }
+}
+
+//Array para String apenas utilizando o toString
+let arrayString = questao1.toString();
+console.log("\nQuestão5: \nArray da questão1: ", questao1, "\nUnir em String: ",arrayString);
+
